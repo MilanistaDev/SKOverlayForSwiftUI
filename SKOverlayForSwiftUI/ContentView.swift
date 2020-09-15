01b637ab41e8efd2c35505e68c82a4dbad051eb5
Takuya Aso <milanista224@gmail.com>
2020-09-16 06:42:05 +0900

    Implemented SKOverlay.

--- SKOverlayForSwiftUI/ContentView.swift
+++ SKOverlayForSwiftUI/ContentView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    @State private var showSKOverlay = false
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("オススメアプリを表示") {
+            self.showSKOverlay.toggle()
+        }
+        .appStoreOverlay(isPresented: $showSKOverlay) {
+            SKOverlay.AppConfiguration(appIdentifier: "1234567890", position: .bottomRaised)
+        }
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
