49977f7a90aae1e7463eaab427f4ec409bd5b903
Takuya Aso <milanista224@gmail.com>
2020-09-16 05:44:40 +0900

    Initial Commit

---
+++ SKOverlayForSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SKOverlayForSwiftUI
+//
+//  Created by Takuya Aso on 2020/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
